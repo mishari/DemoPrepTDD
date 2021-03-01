4bcf81aec239da505cab1f4e3e521353af5a15c0
Mishari Muqbil <mishari@mishari.net>
2021-03-01 16:46:25 +0700

    Factors of two and three working

---
+++ src/factor_primes.py
@@ -0,0 +1,6 @@
+
+def prime_factors(n):
+    if n == 2:
+        return [2]
+    elif n == 3:
+        return [3]
---
+++ src/tests/test_factor_primes.py
@@ -0,0 +1,7 @@
+from factor_primes import *
+
+def test_factorize_two():
+    assert prime_factors(2) == [2]
+
+def test_factorize_three():
+    assert prime_factors(3) == [3]
