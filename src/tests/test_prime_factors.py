d1de36218ca7ed2b81cf412a1f0ecfad41106751
Mishari Muqbil <mishari@mishari.net>
2021-03-02 15:03:26 +0700

    removed tests for is_prime

--- src/tests/test_prime_factors.py
+++ src/tests/test_prime_factors.py
@@ -18,14 +18,14 @@ def test_factorize_six():
 def test_factorize_big_number():
     assert p.prime_factors(3 * 3 * 5) == [3,3,5]
 
-def test_is_two_prime():
-    assert p.is_prime(2) == True
+# def test_is_two_prime():
+#     assert p.is_prime(2) == True
 
-def test_is_three_prime():
-    assert p.is_prime(3) == True
+# def test_is_three_prime():
+#     assert p.is_prime(3) == True
 
-def test_is_four_prime():
-    assert p.is_prime(4) == False
+# def test_is_four_prime():
+#     assert p.is_prime(4) == False
 
-def test_is_five_prime():
-    assert p.is_prime(5) == True
+# def test_is_five_prime():
+#     assert p.is_prime(5) == True
